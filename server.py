86766f931c0a6e35f0df70e973a4f40a892fc7a9
Najm Sheikh <hello@najmsheikh.me>
2016-11-13 08:33:00 -0500

    Code cleanup

--- server.py
+++ server.py
@@ -1,14 +1,9 @@
-# from fbrecog import recognize
-# path = 'group.jpg' #Insert your image file path here
-# access_token = 'EAAMZBwQ6lUVgBANVxZCcbx3HpsMszZCRBxcmt49R0jorlPeumncqI2X6kiYUopYsxTKUovPoPLgkxE7ZCivM1umsZBf3so1z15hLeQwCfbfgAmKjSyakhIEvXTbFoNGgVwtZC1SZAH5WyeTOXegt1DZCKyN9IeVSUDC0LJpAZBmdusgZDZD' #Insert your access token obtained from Graph API explorer here
-# cookie = '***REMOVED***' #Insert your cookie string here
-# fb_dtsg = '***REMOVED***' #Insert the fb_dtsg parameter obtained from Form Data here.
-# print(recognize(path,access_token,cookie,fb_dtsg))
-
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+
 from fbrecog import recognize
 from PIL import Image
+
 import uuid
 import json
 import io
@@ -16,48 +11,60 @@ import io
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-responseData = []
+response_data = []
+
 
 def recognize_image(imagepath):
-	path = imagepath #Insert your image file path here
-	access_token = '***REMOVED***' #Insert your access token obtained from Graph API explorer here
-	cookie = '***REMOVED***' #Insert your cookie string here
-	fb_dtsg = '***REMOVED***' #Insert the fb_dtsg parameter obtained from Form Data here.
-	responseData = recognize(path,access_token,cookie,fb_dtsg)
-	if not responseData:
-		socketio.emit('new_message', {'new_message': 'Sorry, I could not recognize this person.'})
-	else:
-		print(responseData)
-		if len(responseData) == 1:
-			location = ''
-			if responseData[0]['x'] <= 40:
-				location = 'left'
-			elif responseData[0]['x'] > 40 and responseData[0]['x'] <= 60:
-				location = 'center'
-			else:
-				location = 'right'
-			name = responseData[0]['name']
-			speechResponse = 'You have ' + name + ' on your ' + location
-			socketio.emit('new_message', {'found': speechResponse})
-		else:
-			arr = []
-			for i in range(0,len(responseData)):
-				location = ''
-				if responseData[i]['x'] <= 40:
-					location = 'left'
-				elif responseData[i]['x'] > 40 and responseData[i]['x'] <= 60:
-					location = 'center'
-				else:
-					location = 'right'
-				arr.append({'name': responseData[i]['name'], 'location': location })
-			speechResponse = 'You have ' + arr[0]['name'] + ' on your ' + arr[0]['location'] + ' and ' + arr[1]['name'] + ' on your ' + arr[1]['location']
-			socketio.emit('new_message', {'found': speechResponse})
+    path = imagepath						# Direct link to the image being recognized
+    access_token = '***REMOVED***'			# Access token from the Graph API explorer
+    cookie = '***REMOVED***'				# Facebook session cookie for the target account
+    fb_dtsg = '***REMOVED***'				# Form data parameter -- extracted from sample request
 
+    response_data = recognize(path, access_token, cookie, fb_dtsg)
+
+    if not response_data:
+        socketio.emit('new_message', {
+                      'new_message': 'Sorry, I could not recognize this person.'})
+    else:
+        # Internal logging
+        print(response_data)
+
+        # If one person is recognized
+        if len(response_data) == 1:
+            location = ''
+            if response_data[0]['x'] <= 40:
+                location = 'left'
+            elif response_data[0]['x'] > 40 and response_data[0]['x'] <= 60:
+                location = 'center'
+            else:
+                location = 'right'
+            name = response_data[0]['name']
+            speech_response = 'You have ' + name + ' on your ' + location
+            socketio.emit('new_message', {'found': speech_response})
+
+        # If multiple people are recognized
+        # TODO: Fix the hacky assigment
+        else:
+            arr = []
+            for i in range(0, len(response_data)):
+                location = ''
+                if response_data[i]['x'] <= 40:
+                    location = 'left'
+                elif response_data[i]['x'] > 40 and response_data[i]['x'] <= 60:
+                    location = 'center'
+                else:
+                    location = 'right'
+                arr.append({'name': response_data[i][
+                           'name'], 'location': location})
+            speech_response = 'You have ' + arr[0]['name'] + ' on your ' + arr[0][
+                'location'] + ' and ' + arr[1]['name'] + ' on your ' + arr[1]['location']
+            socketio.emit('new_message', {'found': speech_response})
 
 
 @app.route("/")
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
 
 @socketio.on('send_message')
 def handle_source(json_data):
@@ -65,16 +72,17 @@ def handle_source(json_data):
     # text = json_data['message'].encode('ascii', 'ignore')
     # socketio.emit('echo', {'echo': 'test.jpg'})
 
+
 @socketio.on('send_image')
 def image_sent(raw):
-	fileName = str(uuid.uuid4())
-	enrollmentMessage = 'New image enrolled: ' + fileName
-	print(enrollmentMessage)
-	img = Image.open(io.BytesIO(raw))
-	path = fileName+'.jpg'
-	img.save(path)
-	# print(path)
-	recognize_image(path)
+    fileName = str(uuid.uuid4())
+    enrollmentMessage = 'New image enrolled: ' + fileName
+    print(enrollmentMessage)
+    img = Image.open(io.BytesIO(raw))
+    path = fileName + '.jpg'
+    img.save(path)
+    # print(path)
+    recognize_image(path)
 
 
 if __name__ == "__main__":
